dceb9f7464e2c764c5f37142a9dd28fa1986b5af
Edward <cololacalempira5@gmail.com>
2026-08-03 10:26:21 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const config = {
   botName: "Baby Ania",
   version: "1.0.0",
-  creator: "Yulieth",
+  creator: "Yulieth & Duan Ed",
   sessionFolder: "./sessions/main",
   subBotsFolder: "./sessions/subbots",
   ownerNumber: "50400000000",
